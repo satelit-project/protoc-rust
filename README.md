a5b8c809b3eba11b44ae72be08b6f135087ed4f7
Igor Nikitin <rabbitinspace@icloud.com>
2019-07-20 16:51:23 +1000

    generate protos

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # protoc-rust
-Protoc plugin for generating Rust souces
+`protoc` plugin for Rust sources generation
+
+## Dependencies
+
+- Rust (latest _stable_)
+- Protobuf (_v.3.7.1_)
+
+## Instalation
+
+Plugin can be installed via `cargo install`: 
+
+```
+cargo install \
+  --git "https://github.com/satelit-project/protoc-rust" \
+  --force
+```
+
+**TODO**: will it install latest version?
+
+You can also specify custom binary location via `--root` argument.
+
+## Usage
+
+Invoke `protoc` directly:
+
+```
+protoc \
+  --plugin=protoc-gen-rust="$(which protoc-gen-rust)" \
+  --proto_path=<protos include path> \
+  --rust_out=<output directory> \
+  <proto file>...
+```
+
+Or use custom tools like [`prototool`](https://github.com/uber/prototool) like in [`satelit-proto`](https://github.com/satelit-project/satelit-proto) project.
---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+use prost_build::{CodeGeneratorRequest, CodeGeneratorResponse};
+use prost_build::Config as ProtoConfig;
+use tower_grpc_build::ServiceGenerator;
+
+pub fn generate_response(request: CodeGeneratorRequest) -> std::io::Result<CodeGeneratorResponse> {
+    let config = Config::from_request(&request);
+
+    let mut service_gen = ServiceGenerator::default();
+    service_gen.enable_client(config.gen_grpc_client);
+    service_gen.enable_server(config.gen_grpc_server);
+
+    let mut proto_config = ProtoConfig::new();
+    proto_config.service_generator(Box::new(service_gen));
+
+    let response = proto_config.compile_request(request)?;
+    Ok(modularize_response(response))
+}
+
+fn modularize_response(response: CodeGeneratorResponse) -> CodeGeneratorResponse {
+    response
+}
+
+/// Code generation configuration
+struct Config {
+    gen_grpc_client: bool,
+    gen_grpc_server: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            gen_grpc_client: false,
+            gen_grpc_server: false,
+        }
+    }
+}
+
+impl Config {
+    /// Parses configuration from request parameters
+    ///
+    /// To pass custom parameters you need to specify `--rust_opts` argument
+    /// for `protoc` compiler. Multiple parameters may be passed by separating
+    /// them via comma.
+    fn from_request(request: &CodeGeneratorRequest) -> Self {
+        let mut conf = Self::default();
+        let opts = request.parameter.as_ref()
+            .map(|s| s.as_str())
+            .unwrap_or("");
+
+        for opt in opts.split(',') {
+            match opt {
+                "grpc" => {
+                    conf.gen_grpc_client = true;
+                    conf.gen_grpc_server = true;
+                }
+                "grpc-client" => conf.gen_grpc_client = true,
+                "grpc-server" => conf.gen_grpc_server = true,
+                _ => (),
+            }
+        }
+
+        conf
+    }
+}
